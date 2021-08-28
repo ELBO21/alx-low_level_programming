f19f3d7279eccf7b89cebce3ffd9a5e3ade49c5f
ELBO21 <elbetelmengstu@gmail.com>
2021-08-28 11:01:21 -0700

    A c code prints single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+  
+#include <stdio.h>
+
+/**
+ *main - starting point
+ *
+ *Return: return 0 (success)
+ *
+ *Description: prints all single digit numbers (0-9)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	printf("%d", num);
+	printf("\n");
+
+	return (0);
+}
