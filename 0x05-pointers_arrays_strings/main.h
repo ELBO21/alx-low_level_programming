4083792b7ba63b5d01ea05747ce9191a45af6a3e
ELBO21 <elbetelmengstu@gmail.com>
2021-09-07 05:55:32 -0700

    add function proto type to header

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,19 +3,4 @@ void reset_to_98(int *n);
 int _strlen(char *s)i;
 void swap_int(int *a, int *b);
 void _puts(char *str);
-#include "main.h"
-/**
- *_puts - Prints a string
- *@str: input string
- *Return: Nothing
- */
-
-void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
-}
+void rev_string(char *s);
