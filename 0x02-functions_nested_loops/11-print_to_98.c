c1daeb6a905666053f819b6a317749f4c36ebadb
ELBO21 <elbetelmengstu@gmail.com>
2021-09-09 08:41:07 -0700

    Add c-code that natural numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_to_98 - Prints all natural numbers n to 98
+ *
+ *@n: integer
+ *
+ *Return: No
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("98\n");
+}
