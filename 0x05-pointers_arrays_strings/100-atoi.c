7cd2d1b897457cf3c54cf3fcd316711b9b489535
ELBO21 <elbetelmengstu@gmail.com>
2021-09-09 03:29:26 -0700

    Add c code converts string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,28 +8,28 @@
 int _atoi(char *s)
 {
 	int i;
-	int h, p;
+	int b, a;
 
-	h = 0;
-	p = -1;
+	b = 0;
+	a = -1;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
-			p *= -1;
+			a *= -1;
 
 		if (s[i] > 47 && s[i] < 58)
 		{
-			if (h < 0)
-				h = (h * 10) - (s[i] - '0');
+			if (b < 0)
+				b = (b * 10) - (s[i] - '0');
 			else
-				h = (s[i] - '0') * -1;
+				b = (s[i] - '0') * -1;
 
 			if (s[i + 1] < 48 || s[i + 1] > 57)
 				break;
 		}
 	}
-	if (p < 0)
-		h *= -1;
+	if (a < 0)
+		b *= -1;
 
-	return (h);
+	return (b);
 }
