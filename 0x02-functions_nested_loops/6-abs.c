02842a3ae7c8511ffbfafc87ef4422563f4c07cc
ELBO21 <elbetelmengstu@gmail.com>
2021-09-09 05:41:13 -0700

    Add c-code for absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_abs - Computes the absolute value of an integer
+ *
+ *@n: Input intiger
+ *
+ *Return: (n) or (-n)
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= 45;
+	return (n);
+	}
+	else
+		return (n);
+}
